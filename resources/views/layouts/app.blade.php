d13f423386380566077202803c91d5666d9e786d
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-24 18:27:29 +0200

    [UPD]Add header

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/js/app.js'])
+</head>
+
+<body>
+
+    <header>
+        @include('layouts.partials.header')
+    </header>
+
+    <main>
+        @include('layouts.partials.main')
+    </main>
+
+    <footer>
+        @include('layouts.partials.footer')
+    </footer>
+</body>
+
+</html>
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,50 @@
+<header>
+    <div class='container'>
+        <div class='row'>
+            <div class='col-2'>
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logoDC">
+            </div>
+            <div class='col-8 align-self-center'>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+                    <li>
+                        <a href="#">comics</a>
+                    </li>
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+                    <li>
+                        <a href="#">shop</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-2 align-self-center">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i
+                            class="bi bi-search"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
