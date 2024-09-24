69f818dbb300b33bc7d9d673353e14a1ab9402bd
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-24 18:49:35 +0200

    [UPD]Add footer

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,86 @@
+<div class="bg-img">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 d-flex py-3">
+                <div class="col">
+                    <div class="content">
+                        <h3 class="text-light">DC COMICS</h3>
+                        <ul class="text-secondary list-unstyled">
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                    </div>
+                    <div class="content">
+                        <h3 class="text-light">SHOP</h3>
+                        <ul class="text-secondary list-unstyled">
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="content">
+                        <h3 class="text-light">DC</h3>
+                        <ul class="text-secondary list-unstyled">
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy(New)</li>
+                            <li>Ad Choices</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact us</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="content">
+                        <h3 class="text-light">SITES</h3>
+                        <ul class="text-secondary list-unstyled">
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 logo-container">
+                <div class="content">
+                    <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" class="logo" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="bg-footer">
+    <div class="container">
+        <div class="row py-4">
+            <div class="col-6">
+                <div class="content">
+                    <button class="btn text-light">SIGN-UP NOW!</button>
+                </div>
+            </div>
+            <div class="col-6 align-content-center">
+                <div class="content d-flex justify-content-center">
+                    <h4 class="text-primary m-0 mx-4">FOLLOW US</h4>
+                    <div>
+                        <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                        <img src="{{ Vite::asset('resources/img/footer-twitter.png') }} "alt="">
+                        <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                        <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}"alt="">
+                        <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
